d03da0d87fca70882b198e854b7215699a2ec512
kakeru <hkakeru2000@gmail.com>
2021-11-22 14:57:29 +0900

    sample codes

--- NewNews/getDataFromServer.py
+++ NewNews/getDataFromServer.py
@@ -2,6 +2,22 @@
 
 import socket
 
+
+
+IP_ADDRESS = "133.242.175.169"
+PORT = "56747"
+
+
+
 def get_data_from_server():
     """サーバからデータを取得する"""
-    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP_ADDRESS, PORT))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    get_data_from_server()
--- NewNews/sendCliant.py
+++ NewNews/sendCliant.py
@@ -3,3 +3,19 @@
 
 import socket
 
+
+
+IP_ADDRESS = "133.242.175.169"
+PORT = "56747"
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((IP_ADDRESS, PORT))  # IPとポート番号を指定します
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("Welcome to the server!", 'utf-8'))
+    clientsocket.close()
