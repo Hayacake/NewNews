a296149fc1fe43d8f094be6980cfba4adff3488a
kakeru <hkakeru2000@gmail.com>
2021-11-22 16:21:23 +0900

    TCP

--- NewNews/getDataFromServer.py
+++ NewNews/getDataFromServer.py
@@ -1,23 +1,32 @@
 # getDataFromServer.py - サーバからデータをダウンロードする
 
-import socket
+import pickle, socket
+from typing import List, Dict
 
 
 
 IP_ADDRESS = "133.242.175.169"
 PORT = 56747
+BUFFER_SIZE = 4096
 
 
 
-def get_data_from_server():
+def get_data_from_server() -> str:
     """サーバからデータを取得する"""
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((IP_ADDRESS, PORT))
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    fullData = b""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # サーバーに接続を要求する
+        s.connect((IP_ADDRESS, PORT))
+        s.settimeout(180)
+        # サーバーからのデータを受信
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            if len(data) <= 0:
+                break
+            fullData += data
 
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
+        return fullData
 
 
 if __name__ == "__main__":
-    get_data_from_server()
+    print(pickle.loads(get_data_from_server()))
--- NewNews/sendCliant.py
+++ NewNews/sendCliant.py
@@ -1,21 +1,31 @@
 #! /usr/bin/python3
 # sendCliant.py - send json data to cliant
 
-import socket
+import socket, os, pickle, json
 
 
 
 IP_ADDRESS = "133.242.175.169"
 PORT = 56747
+BUFFER_SIZE =  1048576
+DIRNAME = os.path.dirname(__file__)
 
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((IP_ADDRESS, PORT))  # IPとポート番号を指定します
-s.listen(5)
-
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", 'utf-8'))
-    clientsocket.close()
+# Socketの作成
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # IP Adress とPort番号をソケット割り当てる
+    s.bind((IP_ADDRESS, PORT))
+    # Socketの待機状態
+    s.listen(5)
+    # while Trueでクライアントからの要求を待つ
+    while True:
+        # 要求があれば接続の確立とアドレス、アドレスを代入
+        conn, addr = s.accept()
+        conn.settimeout(60)
+        
+        # JSONデータを読み込む
+        payload = json.load(open(DIRNAME + "/data/qiitaNewItems.json"))
+        pay = pickle.dumps(payload)
+        # データを送信する
+        conn.sendall(pay)
