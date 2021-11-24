efeb30a3f638820080d25811d569ce10ebcf0c39
kakeru <hkakeru2000@gmail.com>
2021-11-24 09:08:22 +0900

    working TCP programms

--- NewNews/getDataFromServer.py
+++ NewNews/getDataFromServer.py
@@ -1,8 +1,9 @@
 # getDataFromServer.py - サーバからデータをダウンロードする
 
-import pickle, socket
+import pickle, socket, logging, time
 from typing import List, Dict
 
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 IP_ADDRESS = "133.242.175.169"
@@ -13,22 +14,50 @@ BUFFER_SIZE = 4096
 
 def get_data_from_server() -> List[Dict]:
     """サーバからデータを取得する"""
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(60)
+        logging.info("start TCP")
+        # サーバに接続を要求する
+        s.connect((IP_ADDRESS, PORT))
+
+        # サーバからデータを受信する
+        fullData = b""
+        logging.info("start receiving")
+
+        size = s.recv(BUFFER_SIZE).decode("utf-8")
+        logging.info(size)
+
+        while True:
+            data = s.recv(BUFFER_SIZE)
+            logging.info("receive {} byte: {} ~ {}".format(len(data), data[:5], data[-5:]))
+            fullData += data
+            # 終了のチェック
+            if len(fullData) == int(size):
+                logging.info("end of receiving: {} byte".format(len(fullData)))
+                break
+            logging.info("have received {} byte".format(len(fullData)))
+        
+
+
+        """
         # サーバーからのデータを受信
-        s.sendto(b"connection", (IP_ADDRESS, PORT))
+        s.sendto(b"connect", (IP_ADDRESS, PORT))
 
         # データを受信
         fullData = b""
         while True:
             data, addr = s.recvfrom(BUFFER_SIZE)
-            if data == b"finish":
+            logging.info("get {} from {}".format(data[:5], addr))
+            if data == b"":
+                logging.info("end receiving")
                 break
             fullData += data
-            s.sendto(b"received", (IP_ADDRESS, PORT))
+            print(s.sendto(b"received", addr))
+        """
 
         return pickle.loads(fullData)
 
 
 if __name__ == "__main__":
-    print(get_data_from_server())
+    print(get_data_from_server()[1])
+    print("finish")
--- NewNews/sendCliant.py
+++ NewNews/sendCliant.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 # sendCliant.py - send json data to cliant
 
-import socket, os, pickle, json, time
+import socket, os, pickle, json, logging, time
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 
@@ -12,29 +14,52 @@ DIRNAME = os.path.dirname(__file__)
 
 
 # Socketの作成
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # IP Adress とPort番号をソケット割り当てる
     s.bind((IP_ADDRESS, PORT))
+    logging.info("start listening")
+    # socketの待機状態
+    s.listen(5)
 
     # while Trueでクライアントからの要求を待つ
     while True:
         # 要求があれば接続の確立とアドレス、アドレスを代入
-        msg, addr = s.recvfrom(BUFFER_SIZE)
+        conn, addr = s.accept()
+        logging.info("connect with {}; {}".format(conn, addr))
 
         data = json.load(open(DIRNAME + "/data/qiitaNewItems.json"))
+        # data = {"food": "Apple", "count": 4}
         data = pickle.dumps(data)
+        logging.info("data size is {}".format(len(data)))
 
+        size = len(data)
+        size = str(size)
+        conn.sendall(bytes(size, "utf-8"), socket.MSG_WAITALL)
+
+        # データを送信する
+        startTime = time.time()
+        conn.sendall(data)
+        logging.info("finish sending; {} sec".format(time.time() - startTime))
+        logging.info("=================================")
+
+
+        """
         # 分割して送る
         n = 0
         print(len(data))
+        logging.info("start sending data")
         while True:
-            payload = data[n * 4000: (n + 1) * 4000]
+            payload = data[n * 1000: (n + 1) * 1000]
+            logging.info("{}; {}; {}".format(n, payload[:5], addr))
             # 送るデータがなくなったと伝える
             if payload == b"":
-                s.sendto(b"finish", addr)
+                s.sendto(b"", addr)
+                logging.info("end sending data")
+                logging.info("=====================")
                 break
             s.sendto(payload, addr)
             n += 1
             # time.sleep(0.25)
-            d, a = s.recvfrom(BUFFER_SIZE)
+            d, addr = s.recvfrom(BUFFER_SIZE)
+            """
        
